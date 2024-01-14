7891f4e7ec7d2cb97699541b33ca568fa2dec31f
MargaroneGiada <MRGGRS01R62A494A@studium.unict.it>
2024-01-14 13:24:25 +0100

    Added Laravel and SpringBoot projects

---
+++ Esercitazioni/Laravel_php/prova/resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    Here there are all the contacts:
+    <br> - <a href="https://www.instagram.com/giada_margarone/"> @giada_margarone </a>
+    <br> -
+    <br> -
+    <br> -
+    <br> 
+    <a href="/"> Go back home. </a>
+</body>
+
+</html>
---
+++ Esercitazioni/Laravel_php/prova/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+@yield('contenuto')
+<body>
+    <h3>Hello</h3>
+    Contact me <a href="/contact"> here </a>
+</body>
+</html>
